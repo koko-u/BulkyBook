de747a11d792027becfdfc8aecaebb35fdc55300
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2022-11-06 20:02:07 +0900

    create CoverType ViewModel and operation service

---
+++ BulkyBook.BusinessCore/Services/ICoverTypesService.cs
@@ -0,0 +1,20 @@
+﻿using BulkyBook.Presentation.Result;
+using BulkyBook.Presentation.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.BusinessCore.Services;
+
+public interface ICoverTypesService
+{
+    Task<IEnumerable<CoverTypeViewModel>> GetAllCoverTypesAsync();
+
+    Task<ResponseData<CoverTypeViewModel>> CreateNewCoverTypeAsync(
+        CreateCoverTypeViewModel createCoverType);
+
+    Task<ResponseData<CoverTypeViewModel>> GetSingleCoverTypeByIdAsync(Guid id);
+
+    Task<ResponseData<CoverTypeViewModel>> UpdateCoverTypeAsync(
+        EditCoverTypeViewModel editCoverType);
+
+    Task<ResponseData> DeleteCoverTypeByIdAsync(Guid id);
+}
